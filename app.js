78420e0293f9bc4baa2ab45421129becb0ec15ac
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2017-12-07 18:18:04 -0700

    Added Objects

---
+++ app.js
@@ -0,0 +1,18 @@
+$("#player1").keyup(function(a) {
+    if (a.keyCode === 97) {}
+    	$("#player1").animate({left: '10px'});
+
+
+
+
+
+
+//$("player2").keyup(function(){
+     
+//     $("#player2").animate({left: '10px'});
+// });
+
+
+// $("button").click(function(){
+//     $("#red").animate({left: '250px'});
+// });
