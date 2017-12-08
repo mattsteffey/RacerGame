95153c74f730148220e043db463b4bd5025025e7
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2017-12-07 19:55:15 -0700

    GOT SOME SHIT TO WORK

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
-var pos1 = [0];
-var pos2 = [0];
 
 $(document).keypress(function(a) {
-    if(a.which == 97)
-    console.log("player1");
+    if(a.keyCode == 97)
+	$("#player1").animate({marginLeft: "+=100px"});
+		$pos1 += 1;
 	
- $("#player1").animate({width: "+=100px"});
 });
 
 
 $(document).keypress(function(l) {
-    if(l.which == 108)
-    console.log("player2");
- $("#player2").animate({height: "+=100px"});
+    if(l.keyCode == 108)
+	 $("#player2").animate({marginLeft: "+=100px"});
+	
 });
-
-
-
-
-/*
-$(document).keypress(function(a) {
-    if(a.which == 97)
-    console.log("player1");
- $("#player1").animate({width: "+=100px"});
-});
-
-
-$(document).keypress(function(l) {
-    if(l.which == 108)
-    console.log("player2");
- $("#player2").animate({width: "+=100px"});
-});
-*/
