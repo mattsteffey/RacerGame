2c09f0cd54f857baa5e22069af8c4aa66cd79aa9
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2017-12-07 19:29:43 -0700

    Basically Nothing

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-$(window).keypress(function(a) {
+var pos1 = [0];
+var pos2 = [0];
+
+$(document).keypress(function(a) {
+    if(a.which == 97)
+    console.log("player1");
+	
+ $("#player1").animate({width: "+=100px"});
+});
+
+
+$(document).keypress(function(l) {
+    if(l.which == 108)
+    console.log("player2");
+ $("#player2").animate({height: "+=100px"});
+});
+
+
+
+
+/*
+$(document).keypress(function(a) {
     if(a.which == 97)
     console.log("player1");
  $("#player1").animate({width: "+=100px"});
 });
 
 
-$(window).keypress(function(l) {
+$(document).keypress(function(l) {
     if(l.which == 108)
     console.log("player2");
  $("#player2").animate({width: "+=100px"});
 });
+*/
