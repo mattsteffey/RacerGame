37d47a11c560c8373b0a61bcb55ee56114acde9c
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2017-12-07 20:56:51 -0700

    added win alert

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
+var p1 = 0;
+var p2 = 0;
 
+
+//P1 FUNCTIONALITY
 $(document).keypress(function(a) {
     if(a.keyCode == 97)
-	$("#player1").animate({marginLeft: "+=100px"});
-		$pos1 += 1;
+	$("#player1").animate({marginLeft: "+=50px"}, -200);
 	
 });
 
+$(document).keypress(function(a) {
+	if(a.keyCode == 97)
+     p1 += 1;
+ 	 if (p1 === 20) {alert("Player 1 Wins!");}
 
+});
+
+//P2 FUNCTIONALITY
 $(document).keypress(function(l) {
     if(l.keyCode == 108)
-	 $("#player2").animate({marginLeft: "+=100px"});
+	 $("#player2").animate({marginLeft: "+=50px"}, -200);
 	
 });
+
+$(document).keypress(function(l) {
+	if(l.keyCode == 108)
+     p2 += 1;
+ 	if (p2 === 20) {alert("Player 2 Wins!");}
+
+});
+
