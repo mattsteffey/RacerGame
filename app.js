4a17fc58a39e43ea3df2488015871a6a14457d8b
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2017-12-13 12:03:40 -0700

    added landing html page

--- app.js
+++ app.js
@@ -5,28 +5,28 @@ var p2 = 0;
 //P1 FUNCTIONALITY
 $(document).keypress(function(a) {
     if(a.keyCode == 97)
-	$("#player1").animate({marginLeft: "+=5%"}, -200);
+	$("#player1").animate({marginLeft: "+=3%"}, -200);
 	
 });
 
 $(document).keypress(function(a) {
 	if(a.keyCode == 97)
      p1 += 1;
- 	 if (p1 === 20) {alert("Player 1 Wins!");}
+ 	 if (p1 === 30) {alert("Player 1 Wins!");}
 
 });
 
 //P2 FUNCTIONALITY
 $(document).keypress(function(l) {
     if(l.keyCode == 108)
-	 $("#player2").animate({marginLeft: "+=5%"}, -200);
+	 $("#player2").animate({marginLeft: "+=3%"}, -200);
 	
 });
 
 $(document).keypress(function(l) {
 	if(l.keyCode == 108)
      p2 += 1;
- 	if (p2 === 20) {alert("Player 2 Wins!");}
+ 	if (p2 === 30) {alert("Player 2 Wins!");}
 
 });
 
